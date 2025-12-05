ee9e4c77967950c4c0a804e87e141e0f1d88101c
thiri_sandar <thiris613@gmaail.com>
2025-12-05 20:29:52 +0630

    for home

--- src/App.jsx
+++ src/App.jsx
@@ -679,4 +679,4 @@ function App() {
   )
 }
 
-export default App;
+export default App; 
--- src/pages/UserSite/Home.jsx
+++ src/pages/UserSite/Home.jsx
@@ -64,7 +64,7 @@ const renderMainContent = () => {
             <MemberLoginRegistrationForm
                 onMemberLoginSuccess={onMemberLoginSuccess}
             />
-            <ActiveBusinessDirectory />
+            {/* <ActiveBusinessDirectory /> */}
             {/* If you have ActiveBusinessDirectory here, it only renders when logged out */}
         </>
     );
